3e5700661bca6eb93533678e7ce3b2acd3b151c8
Caleb Voorhees <c.voorhees@setonhill.edu>
2020-02-24 10:28:38 -0500

    Initial Commit

---
+++ Dungeon Escape/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dungeon Escape
+//
+//  Created by Caleb Voorhees on 2/24/20.
+//  Copyright © 2020 Caleb Voorhees. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dungeon EscapeTests/Dungeon_EscapeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dungeon_EscapeTests.swift
+//  Dungeon EscapeTests
+//
+//  Created by Caleb Voorhees on 2/24/20.
+//  Copyright © 2020 Caleb Voorhees. All rights reserved.
+//
+
+import XCTest
+@testable import Dungeon_Escape
+
+class Dungeon_EscapeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
